0bfd34ffddcc6b9ac81b4970b7fec7ab2f33a008
Charlie Burd <cburd@corp.better.site>
2020-11-08 15:07:00 -0700

    deliverable 1/2

--- schema.sql
+++ schema.sql
@@ -70,8 +70,154 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND (hire_date BETWEEN 
 
 -- save data into new csv
 SELECT first_name, last_name
-INTO retirment_info
+INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-SELECT * FROM retirment_info;
+--create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+--check table
+SELECT * FROM retirement_info;
+--joining departments and dept_managers tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+--joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+--create new table that has retirment eligible ppl who are still with company
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE to_date = ('9999-01-01');
+
+SELECT * FROM retirement_info;
+SELECT COUNT(emp_no) FROM current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO employee_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--LIST 1-Employee Information
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+	INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+	 
+--LIST 2-Management
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+	--INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+--LIST 3-Department Retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+	--INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+select * from retirement_info;
+
+--7.3.6 skill drill #1
+SELECT re.emp_no,
+	re.first_name,
+	re.last_name,
+	d.dept_name
+FROM retirement_info as re
+INNER JOIN dept_emp as de
+ON (re.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales';
+
+--7.3.6 skill drill #2
+SELECT re.emp_no,
+	re.first_name,
+	re.last_name,
+	d.dept_name
+FROM retirement_info as re
+INNER JOIN dept_emp as de
+ON (re.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
+
+select * from current_emp;
+
+--DELIVERABLE 1
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO deliverable_pt1
+FROM employees as e
+INNER JOIN titles as t
+ON  (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+select * from deliverable_pt1;
+--DELIVERABLE 1 pt2
+SELECT DISTINCT ON (del.emp_no) del.emp_no,
+	del.first_name,
+	del.last_name,
+	del.title,
+	del.to_date
+INTO unique_titles
+FROM deliverable_pt1 as del
+ORDER BY del.emp_no, del.to_date DESC;
+
+select * from unique_titles;
+
