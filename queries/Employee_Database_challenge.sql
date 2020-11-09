6f83f7b827a9d71c504afaeaff806c198e47dcd2
Charlie Burd <cburd@corp.better.site>
2020-11-08 17:10:14 -0700

    png

---
+++ queries/Employee_Database_challenge.sql
@@ -0,0 +1,51 @@
+--DELIVERABLE 1
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON  (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+select * from deliverable_pt1;
+--DELIVERABLE 1 pt2
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+--DELVIERABLE 1 pt3
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+select * from retiring_titles;
+
+--DELIVERABLE 2 pt1
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility;
--- schema.sql
+++ schema.sql
@@ -201,7 +201,7 @@ SELECT e.emp_no,
 	t.title,
 	t.from_date,
 	t.to_date
-INTO deliverable_pt1
+INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON  (e.emp_no = t.emp_no)
@@ -210,14 +210,38 @@ ORDER BY e.emp_no;
 
 select * from deliverable_pt1;
 --DELIVERABLE 1 pt2
-SELECT DISTINCT ON (del.emp_no) del.emp_no,
-	del.first_name,
-	del.last_name,
-	del.title,
-	del.to_date
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
 INTO unique_titles
-FROM deliverable_pt1 as del
-ORDER BY del.emp_no, del.to_date DESC;
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
 
-select * from unique_titles;
+--DELVIERABLE 1 pt3
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
 
+select * from retiring_titles;
+
+--DELIVERABLE 2 pt1
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility;
